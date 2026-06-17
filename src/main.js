ae65daaddac9c9203becb337899c558f59706fe8
Olga <Olga8086540@mail.ru>
2026-06-17 11:01:01 +0300

    leagues3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // импортируем настроенный роутер
+import vuetify from './plugins/vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
 const app = createApp(App)
+app.use(vuetify)
 app.use(router) // использовать роутер в приложении
 app.mount('#app')
 
